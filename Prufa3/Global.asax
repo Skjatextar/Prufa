bf5697750b59e1c09dab242ecd7027a482d8cb65
Gummi <gudmundure12@ru.is>
2014-04-29 18:32:22 +0000

    Fyrsta prufa

---
+++ Prufa3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prufa3.MvcApplication" Language="C#" %>
---
+++ Prufa3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prufa3.Startup))]
+namespace Prufa3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
