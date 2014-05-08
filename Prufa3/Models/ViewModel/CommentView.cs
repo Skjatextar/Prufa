5fcad1c4bbd0a25aebf5b81b2bf3a29aebebff22
Gummi <gudmundure12@ru.is>
2014-05-08 09:22:20 +0000

    ViewModel búin til

---
+++ Prufa3/Models/ViewModel/CommentView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Prufa3.App_Data.DataAccessLayer;
+
+namespace Prufa3.Models.ViewModel
+{
+    public class CommentView
+    {
+        // Tennging i gagnagrunn - breytist thegar repos. koma inn
+        private SubDataContext db = new SubDataContext();
+
+        public IEnumerable<Prufa3.Models.Entity.Client>  ClientData { get; set; }
+        public IEnumerable<Prufa3.Models.Entity.Comment> CommentData { get; set; }
+        public IEnumerable<Prufa3.Models.Entity.Request> RequestData { get; set; }
+        public IEnumerable<Prufa3.Models.Entity.SubFile> SubFileData { get; set; }
+    }
+}
